edb92d886ce7c8f35fe0dec59f17931fa36b07dd
Link4Electronics <link4electronics@gmail.com>
2026-03-18 17:18:46 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/daggerfall-unity /opt/daggerfall-unity/engine/DaggerfallUnity.x86_64
+quick-sharun ./AppDir/bin/daggerfall-unity ./AppDir/engine/DaggerfallUnity.x86_64
 
 # Additional changes can be done in between here
 
