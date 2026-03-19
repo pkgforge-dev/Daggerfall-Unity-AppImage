efe6e6b9bdc25b5face040a8a84fc076077833a1
Link4Electronics <link4electronics@gmail.com>
2026-03-19 15:04:50 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,8 +15,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/* #./AppDir/bin/daggerfall-unity ./AppDir/bin/engine/DaggerfallUnity.x86_64 ./AppDir/bin/engine/UnityPlayer.so
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
+quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
