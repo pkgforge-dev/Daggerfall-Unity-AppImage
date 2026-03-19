3d79763e23c361582c3ec4ac050c4fce1829ca09
Link4Electronics <link4electronics@gmail.com>
2026-03-19 03:22:45 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,7 +16,7 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package daggerfall-unity-bin
-sed -i "s@DATA_DIR=\"/opt/daggerfall-unity/data\"@DATA_DIR=\"\${APPDIR}/bin/data\"@; s@/opt/daggerfall-unity/engine/DaggerfallUnity.x86_64@./DaggerfallUnity.x86_64@; s@CONFIG_TEMPLATE=\"/usr/share/daggerfall-unity/settings-template.ini\"@CONFIG_TEMPLATE=\"\${APPDIR}/bin/settings-template.ini\"@" /usr/bin/daggerfall-unity
+sed -i "s@DATA_DIR=\"/opt/daggerfall-unity/data\"@DATA_DIR=\"\$APPDIR/bin/data\"@; s@/opt/daggerfall-unity/engine/DaggerfallUnity.x86_64@\$APPDIR/bin/DaggerfallUnity.x86_64@; s@CONFIG_TEMPLATE=\"/usr/share/daggerfall-unity/settings-template.ini\"@CONFIG_TEMPLATE=\"\$APPDIR/bin/settings-template.ini\"@" /usr/bin/daggerfall-unity
 mkdir -p ./AppDir/bin
 mv -v /usr/bin/daggerfall-unity ./AppDir/bin
 mv -v /usr/share/daggerfall-unity/settings-template.ini ./AppDir/bin
